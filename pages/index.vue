251c84ef4396b28ae10b67bb7b3123e1d3ccd1ea
Oskar <oskinavara@gmail.com>
2019-08-26 23:54:08 +0200

    Add routes

--- pages/index.vue
+++ pages/index.vue
@@ -2,27 +2,11 @@
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">
-        tftactics
-      </h1>
-      <h2 class="subtitle">
-        My impeccable Nuxt.js project
-      </h2>
+      <h1 class="title">tftactics</h1>
+      <h2 class="subtitle">My impeccable Nuxt.js project</h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
       </div>
     </div>
   </div>
