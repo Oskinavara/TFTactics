c3254f425b7f649ab7aade0495ebad3ad7932ce7
Oskar <oskinavara@gmail.com>
2019-09-20 21:25:22 +0200

    Add header and color variables

---
+++ components/atoms/Header/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="nav-bar">
+    <nuxt-link to="/champions" class="nav-bar__nav-link">Champions</nuxt-link>
+    <nuxt-link to="/tierlist/champions" class="nav-bar__nav-link">Tier Lists</nuxt-link>
+    <nuxt-link to="/item-builder" class="nav-bar__nav-link">Item Builder</nuxt-link>
+    <nuxt-link to="/team-builder" class="nav-bar__nav-link">Team Builder</nuxt-link>
+    <nuxt-link to="/database" class="nav-bar__nav-link">Database</nuxt-link>
+    <nuxt-link to="/patch-notes" class="nav-bar__nav-link">Patch Notes</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.nav-bar {
+  display: flex;
+  &__nav-link {
+  }
+}
+</style>
--- components/organisms/TheHeader.vue
+++ components/organisms/TheHeader.vue
@@ -1,14 +1,31 @@
 <template>
-  <header>
-    <a href="#">
-      <img src="@/assets/icons/logo.svg" alt="logo">
-    </a>
+  <header class="the-header">
+    <div class="the-header__inner">
+      <main-logo />
+      <nav-bar />
+    </div>
   </header>
 </template>
 
 <script>
-export default {}
+import MainLogo from '@/components/atoms/Header/MainLogo.vue'
+import NavBar from '@/components/atoms/Header/NavBar.vue'
+export default {
+  components: {
+    MainLogo,
+    NavBar
+  }
+}
 </script>
 
 <style lang="scss" scoped>
+.the-header {
+  background-color: $gray;
+  &__inner {
+    display: flex;
+    justify-content: space-between;
+    max-width: 1200px;
+    margin: auto;
+  }
+}
 </style>
